457417069dd046ce675eb8418aee0cd475b1cbae
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-02-19 13:22:54 +0300

    Add input for vector size from user

---
+++ deleteNumberX.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    // Объявляем переменную для размера вектора
+    int n;
+    std::cout << "Input vector size: ";
+    std::cin >> n;
+
+    // Объявляем вектор целых чисел
+    std::vector<int> numbers(n);
