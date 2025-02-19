e0c0a80351e812f396aef0687efbb791e351ce69
WilhelMark <46066617+WilhelMark@users.noreply.github.com>
2025-02-19 13:26:07 +0300

    Implement logic to fill vector with user input numbers

--- deleteNumberX.cpp
+++ deleteNumberX.cpp
@@ -1,7 +1,8 @@
 ﻿#include <iostream>
 #include <vector>
 
-int main() {
+int main()
+{
     // Объявляем переменную для размера вектора
     int n;
     std::cout << "Input vector size: ";
@@ -9,3 +10,20 @@ int main() {
 
     // Объявляем вектор целых чисел
     std::vector<int> numbers(n);
+
+    // Заполняем вектор числами, введенными пользователем
+    std::cout << "Input numbers: ";
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> numbers[i];
+    }
+
+    // Объявляем переменную для числа, которое нужно удалить
+    int x;
+    std::cout << "Input number to delete: ";
+    std::cin >> x;
+
+    // Удаляем все элементы, равные x
+    // Используем алгоритм std::erase_if (C++20 и новее) [1]
+    // Если у вас более старая версия C++, можно использовать цикл и std::vector::erase() [1][7]
+    
